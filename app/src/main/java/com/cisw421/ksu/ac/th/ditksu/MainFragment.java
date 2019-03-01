d8338b55af1232ba9187611cc550b40f72f3fa94
fostkung <bkkfostkung11@gmail.com>
2019-03-01 14:07:35 +0700

    UpDate 3/1/19

--- app/src/main/java/com/cisw421/ksu/ac/th/ditksu/MainFragment.java
+++ app/src/main/java/com/cisw421/ksu/ac/th/ditksu/MainFragment.java
@@ -1,9 +1,7 @@
 package com.cisw421.ksu.ac.th.ditksu;
 
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,7 +17,7 @@ public class MainFragment extends Fragment {
 
     public MainFragment() {
         // Required empty public constructor
-        ImageView img1= getView().findViewById(R.id.img1);
+        ImageView img1 = getView().findViewById(R.id.img1);
         img1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,6 +27,72 @@ public class MainFragment extends Fragment {
             }
 
     });
+//        ข่าวสาร
+        ImageView img2 = getView().findViewById(R.id.img2);
+        img2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.contentMainFragment,
+                        new NewsFragment()).addToBackStack(null).commit();
+
+            }
+
+        });
+//        ตารางเรียน
+        ImageView img3 = getView().findViewById(R.id.img3);
+        img3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.contentMainFragment,
+                        new TableFragment()).addToBackStack(null).commit();
+
+            }
+
+        });
+//        แผนที่
+        ImageView img4 = getView().findViewById(R.id.img4);
+        img4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.contentMainFragment,
+                        new MapFragment()).addToBackStack(null).commit();
+
+            }
+
+        });
+//        ฝลงานสาขา
+        ImageView img5 = getView().findViewById(R.id.img5);
+        img5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.contentMainFragment,
+                        new PortfolioFragment()).addToBackStack(null).commit();
+
+            }
+
+        });
+//        ข้อมูลอาจารย์
+        ImageView img6 = getView().findViewById(R.id.img6);
+        img6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.contentMainFragment,
+                        new TecherFragment()).addToBackStack(null).commit();
+
+            }
+
+        });
+//        ทะเบียน
+        ImageView img7 = getView().findViewById(R.id.img7);
+        img7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.contentMainFragment,
+                        new ThabionFragment()).addToBackStack(null).commit();
+
+            }
+
+        });
     }
 
 
